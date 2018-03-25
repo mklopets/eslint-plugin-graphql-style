50de08779c7d926a4222d6bfdc95dd81fe77e7ef
Marko K <mklopets@gmail.com>
2018-03-25 02:27:48 +0200

    Additional test case

--- tests/lib/rules/graphql-query-nested-newlines.js
+++ tests/lib/rules/graphql-query-nested-newlines.js
@@ -99,6 +99,36 @@ ruleTester.run("graphql-query-nested-newlines", rule, {
                 message: "Nested selection sets should be preceded by an empty line",
                 type: "TaggedTemplateExpression"
             }]
+        },
+        {
+            code: `
+                export default {
+                    query: gql\`
+                        {
+                            me {
+                                id
+                                avatar {
+                                    small
+                                }
+
+                                somethingElse {
+                                    small
+                                }
+                            }
+                        }
+                    \`,
+                    options: {
+                        props: ({ data }) => ({
+                            user: data.me,
+                            isLoading: data.loading
+                        })
+                    }
+                };
+            `,
+            errors: [{
+                message: "Nested selection sets should be preceded by an empty line",
+                type: "TaggedTemplateExpression"
+            }]
         }
     ]
 });
